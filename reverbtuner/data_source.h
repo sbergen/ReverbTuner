4a0edd42c3c1e0809f3b885209a69c0651d8fb81
sbergen <sbergen@50c4564b-4ff0-4a20-a472-0210c9e28819>
2010-03-05 12:18:21 +0000

    added evaluation set, small changes here and there

--- reverbtuner/data_source.h
+++ reverbtuner/data_source.h
@@ -12,15 +12,18 @@ namespace ReverbTuner {
 class DataSource
 {
   public:
+	
+	typedef std::vector<float> Data;
+	
 	DataSource (std::string const & dry_file, std::string const & target_file);
 	~DataSource ();
 	
-	std::vector<float> const & get_dry_data() const { return dry_data; }
-	std::vector<float> const & get_target_data()const { return target_data; }
+	Data const & get_dry_data() const { return dry_data; }
+	Data const & get_target_data()const { return target_data; }
 	
   private:
-	std::vector<float> dry_data;
-	std::vector<float> target_data;
+	Data dry_data;
+	Data target_data;
 };
 
 } // namespace ReverbTuner
