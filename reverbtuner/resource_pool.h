3e3959100b2501d8ac9a9f1fe5b78088e847b507
sbergen <sbergen@50c4564b-4ff0-4a20-a472-0210c9e28819>
2010-03-05 10:58:42 +0000

    Add some initial architecture stuff in the form of headers

---
+++ reverbtuner/resource_pool.h
@@ -0,0 +1,36 @@
+#ifndef REVERB_TUNER_EVALUATION_SCHEDULER_H
+#define REVERB_TUNER_EVALUATION_SCHEDULER_H
+
+namespace ReverbTuner {
+
+class DataSource;
+template<typename T> class EvaluationSet<T>;
+
+/** Base class for a evaluation scheduler, that handles both memory allocation and concurrency
+  * \tparam EvaluatorType evaluator which is use, must be a subclass of \a EvaluationResources
+  */
+template<typename EvaluatorType>
+class EvaluationScheduler
+{
+  public:
+	EvaluationScheduler (DataSource const & data_source)
+	  : data_source (data_source) {}
+	virtual ~EvaluationScheduler () {}
+	
+	/** Allocates resources to run parameter evaluation concurrently
+	  * The allocated resources should be usable across several calls to \a evaluate.
+	  * \param concurrency number of evaluations to run concurrently
+	  */
+	virtual void alloc_resources (int concurrency) = 0;
+	
+	/** Evaluates a set of paramters. Blocks until evaluation is finished.
+	  */
+	virtual void evaluate (EvaluationSet<EvaluatorType> & set) = 0;
+	
+  protected:
+	DataSource const & data_source;
+};
+
+} // namespace ReverbTuner
+
+#endif // REVERB_TUNER_EVALUATION_SCHEDULER_H
