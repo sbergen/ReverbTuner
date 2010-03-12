ccdab866e0f29757634f86f35927aa075783cf61
sbergen <sbergen@50c4564b-4ff0-4a20-a472-0210c9e28819>
2010-03-12 19:58:41 +0000

    Lots of evolutionary related stuff, going to refactor a bit now...

--- reverbtuner/parameter_set.h
+++ reverbtuner/parameter_set.h
@@ -2,8 +2,11 @@
 #define REVERB_TUNER_PARAMETER_SET_H
 
 #include <stdexcept>
+
 #include <boost/ptr_container/ptr_map.hpp>
+
 #include "parameter.h"
+#include "utils.h"
 
 namespace ReverbTuner {
 
@@ -14,10 +17,11 @@ class ParameterSet
   public:
 	
 	typedef boost::ptr_map<unsigned, Parameter> Container;
+	typedef Container::const_iterator iterator;
 	
-	Container::const_iterator begin () const { return parameters.begin(); }
-	Container::const_iterator end () const { return parameters.end(); }
-	Container::const_iterator find (unsigned i) const { return parameters.find (i); }
+	iterator begin () const { return parameters.begin(); }
+	iterator end () const { return parameters.end(); }
+	iterator find (unsigned i) const { return parameters.find (i); }
 	
 	/// Takes ownership of \a parameter
 	void add_parameter (unsigned index, Parameter * parameter)
@@ -25,10 +29,15 @@ class ParameterSet
 	
 	unsigned index_of_parameter (Parameter const * parameter) const
 	{
-		for (Container::const_iterator it = parameters.begin(); it != parameters.end(); ++it) {
+		for (iterator it = parameters.begin(); it != parameters.end(); ++it) {
 			if (it->second == parameter) { return it->first; }
 		}
 		throw std::invalid_argument ("ParameterSet does not have given parameter");
+	}
+	
+	unsigned index_from_random_number (unsigned random) const
+	{
+		return random_from_container (parameters, random)->first;
 	}
 	
   private:
--- reverbtuner/utils.h
+++ reverbtuner/utils.h
@@ -2,6 +2,9 @@
 #define REVERB_TUNER_UTILS_H
 
 #include <cmath>
+#include <iterator>
+
+#include "parameter_modifier.h"
 
 namespace ReverbTuner {
 
@@ -10,6 +13,21 @@ void clamp (T & val, T const & min, T const & max)
 {
 	val = std::max (val, min);
 	val = std::min (val, max);
+}
+
+template<typename Container>
+typename Container::iterator random_from_container (Container & container, unsigned random_uint)
+{
+	random_uint = random_uint % container.size ();
+	typename Container::iterator it = container.begin ();
+	std::advance (it, random_uint);
+	return it;
+}
+
+template<typename Container>
+typename Container::const_iterator random_from_container (Container const & container, unsigned random_uint)
+{
+	return random_from_container (const_cast<Container &> (container), random_uint);
 }
 
 } // namespace ReverbTuner
